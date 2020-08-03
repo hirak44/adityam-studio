a0b0560308dc03a99910f6947bf2034f2556b359
hirak44 <37167069+hirak44@users.noreply.github.com>
2020-08-03 19:25:12 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+(function ($) {
+
+  "use strict";
+
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items: 1,
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+
+
+    $(function() {
+      $('.custom-navbar a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
